765fc6f47d5c842c2c54f28caafaacfc6a5026fc
Daniel Rusnok <daniel.rusnok@itixo.com>
2021-06-23 12:14:50 +0200

    remove commentary

--- AzureFunctions.Presentation.Demo/ReadLicenceFileContent.cs
+++ AzureFunctions.Presentation.Demo/ReadLicenceFileContent.cs
@@ -6,7 +6,6 @@ namespace AzureFunctions.Presentation.Demo
     public static class ReadLicenceFileContent
     {
         // Documentation of BlobTrigger - https://docs.microsoft.com/en-us/azure/azure-functions/functions-bindings-storage-blob-trigger?tabs=csharp#usage
-        //If the file is Text file, we can use string as a file content parameter.
         [FunctionName("ReadLicenceFileContent")]
         public static void Run(
             [BlobTrigger("licenses/{name}", Connection = "AzureWebJobsStorage")] string licenceFileContents, //If the file is Text file, we can use string as a file content parameter.
